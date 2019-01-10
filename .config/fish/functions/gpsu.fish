80baa2e18aaa10741558a1eb2c59e6f1fe0932c9
Jim O'Brien <jim@jimmed.net>
2019-01-10 18:51:48 +0000

    gpsu command passes

--- .config/fish/functions/gpsu.fish
+++ .config/fish/functions/gpsu.fish
@@ -1,4 +1,4 @@
-# Defined in /home/jim/.config/fish/functions/gpsu.fish @ line 2
+# Defined in /tmp/fish.B5L7cl/gpsu.fish @ line 2
 function gpsu
-	gps -u origin (current-branch-name)
+	gps -u origin (current-branch-name) $argv
 end
